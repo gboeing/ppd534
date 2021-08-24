71a6493938681460ae0aa19cf8929185fa16d17c
Geoff Boeing <boeing@usc.edu>
2021-08-23 21:25:45 -0700

    update docker

--- software/docker/docker-build.sh
+++ software/docker/docker-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+docker login
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi $(docker images -q) --force
 docker build -t gboeing/ppd534 .
+docker tag gboeing/ppd534 gboeing/ppd534:latest
 docker run --rm -v "$PWD":/home/jovyan/work gboeing/ppd534:latest /bin/bash -c "conda env export -n base > /home/jovyan/work/environment.yml"
+docker push gboeing/ppd534
