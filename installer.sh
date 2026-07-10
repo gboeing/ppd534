8d00068de0e74d89ba2c47f28e24ad1d203737a9
Geoff Boeing <boeing@usc.edu>
2026-07-10 07:06:30 -0700

    update installer

--- installer.sh
+++ installer.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-curl -s -o pyproject.toml https://raw.githubusercontent.com/gboeing/ppd534/refs/heads/2026/pyproject.toml
-uv pip install -q -r pyproject.toml
-uv pip check
+set -euo pipefail
+
+URL="https://raw.githubusercontent.com/gboeing/ppd534/refs/heads/2026/pyproject.toml"
+
+if [[ -n "$COLAB_RELEASE_TAG" ]]; then
+    echo "Running in Google Colab"
+    curl --fail --silent --show-error --output pyproject.toml "$URL"
+    which uv
+    uv version
+    uv pip install -q -r pyproject.toml
+    uv pip check
+else
+    echo "Not running in Google Colab"
+fi
