bccd4c2d8e0113e3b68c1c8d4561dea8f86ee51a
Geoff Boeing <boeing@usc.edu>
2026-07-10 07:45:35 -0700

    update installer

--- colab-installer.sh
+++ colab-installer.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 URL="https://raw.githubusercontent.com/gboeing/ppd534/refs/heads/2026/pyproject.toml"
 
-if [[ -n "$COLAB_RELEASE_TAG" ]]; then
-    echo "Running in Google Colab"
+if [[ -n "${COLAB_RELEASE_TAG:-}" ]]; then
+    echo "Running in Google Colab."
     curl -LsSf --output pyproject.toml "$URL"
     which uv
     uv self version
@@ -13,5 +13,5 @@ if [[ -n "$COLAB_RELEASE_TAG" ]]; then
     python -c "import osmnx"
     echo "Finished."
 else
-    echo "Not running in Google Colab"
+    echo "Not running in Google Colab."
 fi
